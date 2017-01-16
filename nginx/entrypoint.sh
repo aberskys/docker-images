aefa3256073dcb888a7390913ee01385848b5483
aurimas <aurimas.b@datadog.org>
2017-01-16 13:55:15 +0200

    Added default nginx config and updated Dockerfile

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,5 +6,15 @@
 sed -i '/http {/a \
     server_tokens off;' /etc/nginx/nginx.conf
 
-#save default config
-cp -r /etc/nginx /etc/nginx.default
+for FILE in `ls -A /etc/nginx/site-templates/*.conf`; do
+    # reset template if it is not mounted
+    NAME=$(basename $FILE)
+    if mount | grep "/etc/nginx/sites-enabled/$NAME"; then
+        echo "[INFO] skiping template update: $FILE";
+    else
+        echo "[INFO] updating nginx site template:  /etc/nginx/sites-enabled/$NAME > $FILE";
+        cp -f "$FILE" "/etc/nginx/sites-enabled/$NAME"
+    fi
+done
+
+exec "$@"
